d980d5976b08094f86abe34bf5cbf02b6ca61f54
Andrew Dunai <andunai@gmail.com>
2017-02-23 18:28:28 +0200

    Fixed example

--- example.py
+++ example.py
@@ -13,7 +13,7 @@ def main():
     vie.connect()
 
     vie.set_led_color(VIE.LEDColor.RED)
-    vie.set_eq([6, 1, 0])
+    vie.set_eq([6, 0, -6])
     # vie.set_eq([-6, 0, 6], [VIE.EqBand1.FREQ_125_HZ, VIE.EqBand2.FREQ_250_HZ, VIE.EqBand3.FREQ_16_KHZ])
 
 
